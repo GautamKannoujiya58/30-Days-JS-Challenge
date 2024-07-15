1cadfb85b015e8a30ace9dee8d0e50790a2479ed
gautamKumar <gautamkannoujiya58@gmail.com>
2024-07-15 15:02:23 +0530

    added Featur request and Achievement section

--- Day2_Operators/index.js
+++ Day2_Operators/index.js
@@ -111,3 +111,21 @@ const num5 = 19;
     ? console.log(num5, "is a postive number")
     : console.log(num5, "is an negative number");
 }
+
+// -----------------------------------------------------------------------------------
+
+// Feature Request:
+// 1. Arithmetic Operations Script: Write a script that performs basic arithmetic operations (addition, subtraction, multiplication, division, remainder)
+// on two numbers and logs the results.
+// 2. Comparison and Logical Operators Script: Create a script that compares two numbers using different comparison operators and combines
+// conditions using logical operators, logging the results.
+// 3. Ternary Operator Script: Write a script that uses the ternary operator to determine if a number is positive or negative and logs the result.
+
+// Achievement:
+// By the end of these activities, students will:
+
+// . Understand and use arithmetic operators to perform basic calculations.
+// . Use assignment operators to modify variable values.
+// . Compare values using comparison operators.
+// · Combine conditions using logical operators.
+// . Use the ternary operator for concise conditional expressions.
