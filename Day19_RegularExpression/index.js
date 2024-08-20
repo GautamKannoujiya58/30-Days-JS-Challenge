6fecf3f591cacbcca13552f900a4319c5cc90859
gautamKumar <gautamkannoujiya58@gmail.com>
2024-08-20 15:48:29 +0530

    Day19 added

---
+++ Day19_RegularExpression/index.js
@@ -0,0 +1,6 @@
+// Day 19: Regular Expression
+// Tasks/Activities:
+
+// Activity 1: Basic Regular Expression
+// Task 1: Write a regular expression to match a simple pattern (e.g. match all occurrences of the word "JavaScript" in a string).
+// Log the matches
